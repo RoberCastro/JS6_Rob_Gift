6db6ec115f8965a1a1f1294bc74e9ea23da69396
robercastro <robertoicastro@gmail.com>
2016-12-14 17:11:15 +0100

    Petits changements

--- dev/app/pages/home/home.js
+++ dev/app/pages/home/home.js
@@ -52,24 +52,80 @@ export class HomePage {
             <label for="email">Email:</label> <input type="email" name="email" value="" placeholder="votreemail.ch"  /><br/>
             <label for="password">Password:</label> <input type="password" name="password" value=""  /><br/>
           </p>
+          <a class="btn" onclick="Materialize.toast('I am a toast', 4000)">Toast!</a>
             <button>Login</button>
             <div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
 
             <img id="drag1" src="./src/images/img_logo.gif" draggable="true" ondragstart="drag(event)">
           <div>
-              <!-- Dropdown Trigger -->
-              <a class='dropdown-button btn' href='#' data-activates='dropdown1'>Drop Me!</a>
+            <div>
+                <!-- Dropdown Trigger -->
+                <a class='dropdown-button btn' href='#' data-activates='dropdown1' class="col s6">ham</a>
 
-              <!-- Dropdown Structure -->
-              <ul id='dropdown1' class='dropdown-content'>
-                <li><a href="#!">one</a></li>
-                <li><a href="#!">two</a></li>
-                <li class="divider"></li>
-                <li><a href="#!">three</a></li>
-              </ul>
+                <!-- Dropdown Structure -->
+                <ul id='dropdown1' class='dropdown-content' class="col s6">
+                  <li><a href="#!">Ibérico</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#!">Pata negra</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#!">Jabugo</a></li>
+                </ul>
+            </div>
+            <div>
 
+                <!-- Dropdown Trigger -->
+                <a class='dropdown-button btn' href='#' data-activates='dropdown2' class="col s2">Huile d'olive</a>
+
+                <!-- Dropdown Structure -->
+                <ul id='dropdown2' class='dropdown-content'>
+                  <li><a href="#!">Normal</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#!">Virgen</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#!">Virgen extra</a></li>
+                </ul>
+            </div>
+            <div>
+                <!-- Dropdown Trigger -->
+                <a class='dropdown-button btn' href='#' data-activates='dropdown3' class="col s2">Fromage manchego</a>
+
+                <!-- Dropdown Structure -->
+                <ul id='dropdown3' class='dropdown-content'>
+                  <li><a href="#!">Curado</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#!">Semicurado</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#!">Tierno</a></li>
+                </ul>
+            </div>
+            <div>
+                <!-- Dropdown Trigger -->
+                <a class='dropdown-button btn' href='#' data-activates='dropdown4'>Confisserie</a>
+
+                <!-- Dropdown Structure -->
+                <ul id='dropdown4' class='dropdown-content'>
+                  <li><a href="#!">Turron</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#!">Sobaos</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#!">Polvorones</a></li>
+                </ul>
+            </div>
+            <div>
+                <!-- Dropdown Trigger -->
+                <a class='dropdown-button btn' href='#' data-activates='dropdown5'>Brandy de Jerez</a>
+
+                <!-- Dropdown Structure -->
+                <ul id='dropdown5' class='dropdown-content'>
+                  <li><a href="#!">LA BOTA DE BRANDY Nº 29</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#!">TORRES JAIME I</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#!">PEINADO SOLERA 100 AÑOS</a></li>
+                </ul>
+
+            </div>
           </div>
-          <a class="btn" onclick="Materialize.toast('I am a toast', 4000)">Toast!</a>
 
         </form>
       </section>`;
